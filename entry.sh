7b0a6b22fb8502eca36f7aa521da2a08793f8540
Tim Robinson <tim@panubo.com>
2016-04-18 12:31:59 +1000

    Fix zones directory on container bootstrap

--- entry.sh
+++ entry.sh
@@ -5,9 +5,12 @@
 chown nsd:nsd /etc/nsd/nsd_control.key /etc/nsd/nsd_control.pem /etc/nsd/nsd_server.pem
 
 # Setup config if it doesn't already exist
-[ ! -f "/etc/nsd/nsd.conf"] && cp -a /etc/.nsd_defaults/nsd.conf /etc/nsd/nsd.conf
+[ ! -f "/etc/nsd/nsd.conf" ] && cp -a /etc/.nsd_defaults/nsd.conf /etc/nsd/nsd.conf
+
+# Create zones directory if it doesn't exist
+[ ! -e "/var/lib/nsd/zones" ] && mkdir -p /var/lib/nsd/zones
 
 # Fix nsd directory permissions, this is where it writes its database
-chown nsd:nsd /var/lib/nsd
+chown nsd:nsd /var/lib/nsd /var/lib/nsd/zones
 
 exec $@
